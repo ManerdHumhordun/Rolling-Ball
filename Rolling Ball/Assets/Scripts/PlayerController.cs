3fff9650a2b616ebb19d4b1ee4f433d43cb9bedd
ManerdHumhordun <35775584+ManerdHumhordun@users.noreply.github.com>
2018-01-24 15:42:41 -0600

    Added Rolling Ball Project Files

---
+++ Rolling Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    private Rigidbody rb;
+    private int count;
+    private bool win = true;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+        count = 0;
+
+        SetCountText();
+
+        winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up") || other.gameObject.CompareTag("Pick Up++"))
+        {
+            other.gameObject.SetActive(false);
+
+            if (other.gameObject.CompareTag("Pick Up"))
+                ++count;
+            else
+                count += 5;
+
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 16 && win == true)
+        {
+            winText.text = "You Win!";
+            GameObject.Find("Player").SendMessage("Win");
+        }
+    }
+
+    void Lose()
+    {
+        win = false;
+    }
+}
---
+++ Rolling Ball/Assets/Scripts/Timer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text timerText;
+    public Text loseText;
+    private float startTime;
+    private float timeGiven = 30;
+    private bool complete = false;
+
+	// Use this for initialization
+	void Start () {
+        startTime = timeGiven;
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (complete)
+            return;
+
+        //float t = Time.time - startTime;
+        float t = startTime - Time.time;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+        double sec = (t % 60);
+
+        timerText.text = minutes + ":" + seconds;
+
+        //seconds.Equals("0.00") ||
+
+        if (sec <= 0.00)
+            Lose();
+	}
+
+    public void Win()
+    {
+        complete = true;
+        timerText.color = Color.yellow;
+    }
+
+    public void Lose()
+    {
+        complete = true;
+        timerText.text = "0:0.00";
+        timerText.color = Color.red;
+        loseText.text = "You Lose...";
+        GameObject.Find("Player").SendMessage("Lose");
+    }
+}
